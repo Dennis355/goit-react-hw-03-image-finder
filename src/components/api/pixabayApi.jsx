30a177c9678edfaecdb7b0befeaf21d39a639c8c
Dennis355 <Snidevichden@gmail.com>
2023-04-04 23:05:12 +0300

    fixe

--- src/components/api/pixabayApi.jsx
+++ src/components/api/pixabayApi.jsx
@@ -41,13 +41,3 @@ export const fetchData = async (query, page ) => {
 
 
 
-
-
-// export const fetchData = (query, page, perPage) => {
-//   return axios
-//     .get(
-//       `${BASE_URL}/api/?q=${query}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=${perPage}`
-//     )
-//     .then(response => response.data);
-// };
-
